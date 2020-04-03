7c7b0d51a9287d388983a4b94cbffddf9154c510
yinyinyinyinyin <348029293@qq.com>
2020-04-03 17:47:52 +0800

    aa

---
+++ web21-wx-houniao/pages/main/main.js
@@ -0,0 +1,83 @@
+// pages/main/main.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tuijianArr:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url:'https://www.51houniao.com/product/product/getProductRecommendUser',
+      method:'POST',
+      data:{desc: false,
+        orderBy: "top",
+        pageNow: 1,
+        pageSize: 20,
+        seller_user_id: "4bc4027c645343f09a964b5c2e9f875b"},
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success:(res)=>{
+          console.log(res);
+          this.setData({
+            tuijianArr:res.data
+          })
+        }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
