58d449bf465671ecdd75bfd4559c57e1b5b0b3c3
yinyinyinyinyin <348029293@qq.com>
2020-04-08 15:45:08 +0800

    aa

---
+++ web21-wx-nodebook/pages/plugin/plugin.js
@@ -0,0 +1,87 @@
+// pages/plugin/plugin.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    picsWeblink: [
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/1.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/20.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/19.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/18.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/17.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/16.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/15.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/14.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/13.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/12.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/11.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/10.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/9.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/8.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/7.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/6.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/5.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/4.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/3.jpg/h300",
+      "https://tapp-1251082889.picgz.myqcloud.com/3d/1/2.jpg/h300"
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
